39181d6020165b79ac237fefef96e0bfca188971
Eduarda Buffo <duda.buffo2@gmail.com>
2021-09-03 11:27:46 -0300

    Projeto Criado

---
+++ src/main/java/com/eduarda/mongodb/CursomongodbApplication.java
@@ -0,0 +1,13 @@
+package com.eduarda.mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eduarda/mongodb/CursomongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduarda.mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
